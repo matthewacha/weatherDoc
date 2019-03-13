4bb1ac482f58558e6d6f8319398aac54be61cd31
matthewacha <matthewacha@gmail.com>
2019-03-13 16:02:10 +0300

    Simple weather app

---
+++ README.md
@@ -0,0 +1,22 @@
+### weatherdoc
+
+#### your friendly weather and time reader
+
+#### To use
+
+
+Please ensure that the following are installed:
+* node
+* npm
+* you have an account at `http://www.geonames.org/` and `https://openweathermap.org/api`
+* acquire an api key from `https://openweathermap.org/api` which should be added to the environement variables.
+
+Environment variables
+* Add a .env file and use the format shown in the file `.env.example`
+
+### To run
+
+Run ```npm install```
+
+Then run ```node index```
+
--- index.js
+++ index.js
@@ -1,22 +1,38 @@
-const fetch = require("node-fetch");
+const fetch = require("node-fetch")
+const readIo = require('console-read-write');
 const dotenv = require('dotenv')
 dotenv.config()
 
 
+const main = async () => {
+    readIo.write("Input location and postal code seperated by a space e.g. London uk ");
+    const value = await readIo.read()
+    const cleanData = value.split(' ')
+    getWeatherAndTime(cleanData)
+}
+
 const getWeatherAndTime = async (data) => {
+    let timeUrl, getTime, timeAndWeather, timeDetails;
     const apiKey = `${process.env.OPENWEATHER_API_KEY}`
     const userName = `${process.env.GEONAMES_USERNAME}`
     const weatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${data[0]},${data[1]}&appid=${apiKey}`
-    const weatherData = await fetch(weatherUrl)
+    const weatherData = await fetch(weatherUrl).catch(err=> console.log(err.message))
     const body = await weatherData.json()
+    if(body.cod !== '404'){
+        const { coord: { lat, lon },main, weather, visibility, wind, clouds, dt } = await body
+        timeUrl = `http://api.geonames.org/timezoneJSON?lat=${lat}&lng=${lon}&username=${userName}`
+        getTime = await fetch(timeUrl)
+        timeDetails = await getTime.json()
+        timeAndWeather = {
+            currentTime: timeDetails.time,
+            weather: {
+                weather: weather[0],
+                main, visibility, wind, clouds, dt}}
+        console.log(timeAndWeather)
+    } else {
+        console.log(body.message);
+    }
 
-    const { coord: { lat, lon },main, weather, visibility, wind, clouds, dt } = await body
-    const timeUrl = `http://api.geonames.org/timezoneJSON?lat=${lat}&lng=${lon}&username=${userName}`
-    const getTime = await fetch(timeUrl)
-    const timeDetails = await getTime.json()
-
-    const timeAndWeather = { 'currentTime': timeDetails.time, 'weather': {weather: weather[0], main, visibility,wind, clouds, dt}}
-    console.log(timeAndWeather);
 }
 
-getWeatherAndTime(['Kigali', 'rw'])
+main()
